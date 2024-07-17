13d2df395af402d38d07222b6f621110508c48ff
Гамид Халидов <khalidov-gamid@mail.ru>
2024-07-17 14:33:35 +0300

    Initial Commit

---
+++ Ric&Morty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ric&Morty
+//
+//  Created by Gamıd Khalıdov on 17.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ric&Morty/Ric_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ric_MortyApp.swift
+//  Ric&Morty
+//
+//  Created by Gamıd Khalıdov on 17.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Ric_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
